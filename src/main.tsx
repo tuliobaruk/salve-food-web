3a102f44ce27ea294f95a089352c933eab706013
tuliobaruk <tuliobarukms@gmail.com>
2025-01-23 11:40:52 -0300

    fix: Agora a sidebar usa navegação do router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,11 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import { StrictMode } from "react";
 import { Toaster } from "sonner";
+
 import { AuthProvider } from "@/context/AuthContext";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "@/pages/HomePage";
-import NotFoundPage from "@/pages/NotFoundPage";
-import LoginPage from "@/pages/LoginPage";
-import SingupPage from "@/pages/SingupPage";
-import TestPage from "@/pages/TestPage";
-import Dashboard from "@/pages/Dashboard";
-import Layout from "@/components/global_layout";
-import CreateStorePage from "@/pages/CreateStorePage";
-import Pedidos from "@/pages/PedidosPage";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <HomePage />,
-		errorElement: <NotFoundPage />,
-	},
-	{
-		path: "/login",
-		element: <LoginPage />,
-	},
-	{
-		path: "/singup",
-		element: <SingupPage />,
-	},
-	{
-		path: "/test",
-		element: <TestPage />,
-	},
-	{
-		path: "/dashboard",
-		element: <Dashboard />,
-	},
-	{
-		path: "/pedidos",
-		element: <Pedidos />,
-	},
-	{
-		path: "/createStore",
-		element: <CreateStorePage />,
-	},
-	// Rota Temporária, criei para testar a lógica de refresh dos tokens
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "@/router";
 
 const rootElement = document.getElementById("root")!;
 if (rootElement) {
@@ -56,9 +14,7 @@ if (rootElement) {
 		<StrictMode>
 			<Toaster position="top-center" />
 			<AuthProvider>
-				<Layout>
-					<RouterProvider router={router} />
-				</Layout>
+				<RouterProvider router={router} />
 			</AuthProvider>
 		</StrictMode>,
 	);
---
+++ src/router.tsx
@@ -0,0 +1,59 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// Sidebar
+
+import SidebarLayout from "@/components/global_layout";
+
+// Páginas
+
+import HomePage from "@/pages/HomePage";
+import NotFoundPage from "@/pages/NotFoundPage";
+import LoginPage from "@/pages/LoginPage";
+import SingupPage from "@/pages/SingupPage";
+import TestPage from "@/pages/TestPage";
+import Dashboard from "@/pages/Dashboard";
+import CreateStorePage from "@/pages/CreateStorePage";
+import Pedidos from "@/pages/PedidosPage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+		errorElement: <NotFoundPage />,
+	},
+	{
+		path: "/login",
+		element: <LoginPage />,
+	},
+	{
+		path: "/singup",
+		element: <SingupPage />,
+	},
+	{
+		path: "/test",
+		element: <TestPage />,
+	},
+	{
+		path: "/dashboard",
+		element: (
+			<SidebarLayout>
+				<Dashboard />,
+			</SidebarLayout>
+		),
+	},
+	{
+		path: "/pedidos",
+		element: (
+			<SidebarLayout>
+				<Pedidos />,
+			</SidebarLayout>
+		),
+	},
+	{
+		path: "/createStore",
+		element: <CreateStorePage />,
+	},
+	// Rota Temporária, criei para testar a lógica de refresh dos tokens
+]);
+
+export default router;
