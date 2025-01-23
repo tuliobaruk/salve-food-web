dab333dc4f85221e8767f8129ba27d65a9cae309
tuliobaruk <tuliobarukms@gmail.com>
2025-01-23 12:00:09 -0300

    fix: Removendo useAuth desnecessário da sidebar

--- src/components/global_layout.tsx
+++ src/components/global_layout.tsx
@@ -1,15 +1,13 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import { useAuth } from "@/context/AuthContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-	const { isAuthenticated } = useAuth();
 	return (
 		<SidebarProvider>
 			<div className="flex h-screen w-screen">
-				{isAuthenticated && <AppSidebar />}
+				<AppSidebar />
 				<div className="flex-1 flex flex-col">
-					{isAuthenticated && <SidebarTrigger />}
+					<SidebarTrigger />
 					<main className="flex-1 overflow-auto">{children}</main>
 				</div>
 			</div>
