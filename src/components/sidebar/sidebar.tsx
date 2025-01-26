a724e065b8152aa2840d7b623c19acb93747baa7
tuliobaruk <tuliobarukms@gmail.com>
2025-01-25 21:43:40 -0300

    feat: Novo layout para o header da sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,8 +2,7 @@ import {
 	Sidebar,
 	SidebarContent,
 	SidebarGroup,
-	SidebarGroupContent,
-	SidebarGroupLabel,
+	SidebarHeader,
 	SidebarMenu,
 	SidebarMenuButton,
 	SidebarMenuItem,
@@ -11,16 +10,56 @@ import {
 
 import { Link } from "react-router-dom";
 import { footerItems, topItems } from "@/components/sidebar/sidebar_items";
+import { useLoja } from "@/context/LojaContext";
 
 export default function AppSidebar() {
+	const { loja } = useLoja();
+
 	return (
 		<Sidebar>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size="lg" asChild>
+							<div className="flex items-center gap-4">
+								{loja?.image && (
+									<img
+										src={loja.image}
+										alt={loja.nome}
+										className="w-12 h-12 rounded-lg object-cover bg-gray-200"
+									/>
+								)}
+								<div className="flex flex-col">
+									<span className="font-semibold text-lg text-gray-800">
+										{loja?.nome || "Nome do Restaurante"}
+									</span>
+								</div>
+							</div>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+
 			<SidebarContent>
 				<SidebarGroup>
-					<SidebarGroupLabel>Nome do restaurante</SidebarGroupLabel>
-					<SidebarGroupContent>
+					<SidebarMenu>
+						{topItems.map((item) => (
+							<SidebarMenuItem key={item.title}>
+								<SidebarMenuButton asChild>
+									<Link to={item.url}>
+										<item.icon />
+										<span>{item.title}</span>
+									</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+
+				<div className="mt-auto">
+					<SidebarGroup>
 						<SidebarMenu>
-							{topItems.map((item) => (
+							{footerItems.map((item) => (
 								<SidebarMenuItem key={item.title}>
 									<SidebarMenuButton asChild>
 										<Link to={item.url}>
@@ -31,24 +70,6 @@ export default function AppSidebar() {
 								</SidebarMenuItem>
 							))}
 						</SidebarMenu>
-					</SidebarGroupContent>
-				</SidebarGroup>
-				<div className="mt-auto">
-					<SidebarGroup>
-						<SidebarGroupContent>
-							<SidebarMenu>
-								{footerItems.map((item) => (
-									<SidebarMenuItem key={item.title}>
-										<SidebarMenuButton asChild>
-											<Link to={item.url}>
-												<item.icon />
-												<span>{item.title}</span>
-											</Link>
-										</SidebarMenuButton>
-									</SidebarMenuItem>
-								))}
-							</SidebarMenu>
-						</SidebarGroupContent>
 					</SidebarGroup>
 				</div>
 			</SidebarContent>
