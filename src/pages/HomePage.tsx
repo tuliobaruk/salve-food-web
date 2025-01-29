de5ecf5b9b7f25d0bb88a6461f3688f76dbd0d5c
Lucas Cleiton <lcsf1@discente.ifpe.edu.br>
2025-01-29 11:00:54 -0300

    Alteracao da home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,23 +10,25 @@ import { Instagram } from "lucide-react";
 export default function HomePage() {
 	return (
 		<div className="flex min-h-screen flex-col items-center bg-[#f9f5f1]">
-			<header className="w-full h-20 fixed top-0 left-0 right-0 z-50 flex justify-between items-center max-w-full mx-auto py-6 px-4 md:px-10 bg-[#D9D9D9]">
+
+			<header className="w-full h-20 fixed top-0 left-0 right-0 z-50 flex justify-between items-center max-w-full mx-auto py-6 px-4 md:px-10 bg-[#F9F5F1]/50 backdrop-blur-md">
+
 				<div className="text-2xl font-bold flex items-center gap-2">
 					<img src={salveImage} alt="SalveFood" className="w-16 h-16" />
 					<span>SalveFood</span>
 				</div>
 
 				<nav className="flex gap-6 text-lg">
-					<a href="#" className="text-gray-700 hover:text-gray-900">
+					<a href="#" className="text-gray-700 px-3 py-2 rounded-md transition duration-300 hover:bg-orange-500 hover:bg-opacity-100 hover:text-white">
 						Home
 					</a>
-					<a href="#about" className="text-gray-700 hover:text-gray-900">
+					<a href="#about" className="text-gray-700 px-3 py-2 rounded-md transition duration-300 hover:bg-orange-500 hover:bg-opacity-100 hover:text-white">
 						Sobre
 					</a>
-					<a href="#features" className="text-gray-700 hover:text-gray-900">
+					<a href="#features" className="text-gray-700 px-3 py-2 rounded-md transition duration-300 hover:bg-orange-500 hover:bg-opacity-100 hover:text-white">
 						Recursos
 					</a>
-					<a href="#contact" className="text-gray-700 hover:text-gray-900">
+					<a href="#contact" className="text-gray-700 px-3 py-2 rounded-md transition duration-300 hover:bg-orange-500 hover:bg-opacity-100 hover:text-white">
 						Contato
 					</a>
 				</nav>
