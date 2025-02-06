c40725757ba442e65e114e13ef3031d009df1064
tuliobaruk <tuliobarukms@gmail.com>
2025-02-06 15:34:04 -0300

    feat: refresh após um 403 forbidden

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -31,6 +31,7 @@ axiosInstance.interceptors.response.use(
 		const originalRequest = error.config;
 		if (
 			error.response.status === 401 ||
+			(error.response.status === 403 && !originalRequest._retry) ||
 			(error.response.status === 500 && !originalRequest._retry)
 		) {
 			originalRequest._retry = true;
